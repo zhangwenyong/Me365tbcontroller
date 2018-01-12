0beb7a3f3d2d2171c448bc08f0dc95a5d989c002
张文勇 <>
2018-01-12 11:31:56 +0800

    添加target 类

---
+++ Me365tbcontroller/Target_ME365BaseTabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  Target_ME365BaseTabBarViewController.h
+//  Baseviewcontroller
+//
+//  Created by 张文勇 on 2018/1/12.
+//  Copyright © 2018年 张文勇. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import  <UIKit/UIKit.h>
+@interface Target_ME365BaseTabBarViewController : NSObject
+-(UITabBarController *)Action_GetME365BaseTabBarViewControllerWith:(NSDictionary *)dic;
+@end
---
+++ Me365tbcontroller/Target_ME365BaseTabBarViewController.m
@@ -0,0 +1,17 @@
+//
+//  Target_ME365BaseTabBarViewController.m
+//  Baseviewcontroller
+//
+//  Created by 张文勇 on 2018/1/12.
+//  Copyright © 2018年 张文勇. All rights reserved.
+//
+
+#import "Target_ME365BaseTabBarViewController.h"
+#import "ME365BaseTabBarViewController.h"
+@implementation Target_ME365BaseTabBarViewController
+-(UITabBarController *)Action_GetME365BaseTabBarViewControllerWith:(NSDictionary *)dic
+{
+    ME365BaseTabBarViewController *CV = [[ME365BaseTabBarViewController alloc]init];
+    return CV;
+}
+@end
